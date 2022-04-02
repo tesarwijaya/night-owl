a38ed7e93b44aec23435c62015748577a640972f
Tesar Wijaya <tesarwijaya@gmail.com>
2022-04-02 22:45:16 +0700

    Add postgres sql dependencies

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,6 +7,12 @@ import (
 
 type Config struct {
 	Port string `envconfig:"PORT" default:"8000"`
+
+	SqlDBHost     string `envconfig:"SQL_DB_HOST" default:"night-owl-sql-db"`
+	SqlDBPort     int64  `envconfig:"SQL_DB_PORT" default:"5432"`
+	SqlDBUsername string `envconfig:"SQL_DB_USERNAME" default:"root"`
+	SqlDBPassword string `envconfig:"SQL_DB_PASSWORD" default:"pass"`
+	SqlDBName     string `envconfig:"SQL_DB_NAME" default:"night_owl_db"`
 }
 
 func NewConfig() (*Config, error) {
