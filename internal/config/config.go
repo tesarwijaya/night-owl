450c307e05a4290df4060650ebd8feb83c9f5546
Tesar Wijaya <tesarwijaya@gmail.com>
2022-04-03 11:50:56 +0700

    Modify env to prefix with app

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,11 +8,11 @@ import (
 type Config struct {
 	Port string `envconfig:"PORT" default:"8000"`
 
-	SqlDBHost     string `envconfig:"SQL_DB_HOST" default:"night-owl-sql-db"`
-	SqlDBPort     int64  `envconfig:"SQL_DB_PORT" default:"5432"`
-	SqlDBUsername string `envconfig:"SQL_DB_USERNAME" default:"root"`
-	SqlDBPassword string `envconfig:"SQL_DB_PASSWORD" default:"pass"`
-	SqlDBName     string `envconfig:"SQL_DB_NAME" default:"night_owl_db"`
+	SqlDBHost     string `envconfig:"APP_SQL_DB_HOST" default:"night-owl-sql-db"`
+	SqlDBPort     int64  `envconfig:"APP_SQL_DB_PORT" default:"5432"`
+	SqlDBUsername string `envconfig:"APP_SQL_DB_USERNAME" default:"root"`
+	SqlDBPassword string `envconfig:"APP_SQL_DB_PASSWORD" default:"pass"`
+	SqlDBName     string `envconfig:"APP_SQL_DB_NAME" default:"night_owl_db"`
 }
 
 func NewConfig() (*Config, error) {
